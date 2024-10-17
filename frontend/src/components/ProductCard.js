cb84289f3cd107de9e6e73258792e4022aefaa6b
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-10-17 13:37:00 +0800

    Assignment4

---
+++ frontend/src/components/ProductCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+// import './ProductCard.css';
+
+const ProductCard = ({ product, addToCart }) => {
+
+  return (
+
+    <div className="product-card">
+
+      <img src={product.imageUrl} alt={product.name} />
+
+      <h3>{product.name}</h3>
+
+      <p>₱{product.price}</p>
+
+      <h4>{product.description}</h4>
+
+      <button onClick={() => addToCart(product)}>Add to Cart</button>
+
+    </div>
+
+  );
+
+};
+
+export default ProductCard;
